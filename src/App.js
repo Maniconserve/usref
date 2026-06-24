b66ad58831e6da5bfa4dddeb7dfdb94b0dee68fc
Manideep Vemula <deep05739@gmail.com>
2026-06-24 16:15:45 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
+import { useRef } from 'react';
 
 function App() {
+  let h1ele = useRef("Hello World");
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1 ref={h1ele} className="blue">Using Useref</h1>
+      <button onClick={() => {
+        h1ele.current.classList.toggle("orange");
+      }}>Change</button>
     </div>
   );
 }
